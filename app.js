a95d9efed97ecb4817587fbf902dc4907f1c4b1e
Jona Reyes <jonamreyes@gmail.com>
2021-03-01 09:58:03 -0500

    more vue basic concepts

--- app.js
+++ app.js
@@ -3,10 +3,41 @@ const app = Vue.createApp({
 	//template: '<h2> I am the template </h2>'
 	data(){
 		return {
+			url: "https://google.com",
 			showBooks: true,
 			title: "The Final Empire",
 			author: "Brandon Sanderson",
-			age: 45
+			age: 45,
+
+			//for mousemove
+			x: 0,
+			y: 0,
+
+			books: [
+
+				{ 
+					title: 'name of the wind',
+					author: 'patrick rothfuss',
+					img: 'assets/1.jpeg',
+					isFav: true
+				},
+
+				{ 
+					title: 'the way of kings',
+					author: 'Brandon Sanderson',
+					img: 'assets/2.jpeg',
+					isFav: false
+				},
+
+				{ 
+					title: 'the final empire',
+					author: 'Brandon Sanderson',
+					img: 'assets/3.jpeg',
+					isFav: true
+				}
+
+			]
+
 		}
 	},
 	methods:{
@@ -17,6 +48,25 @@ const app = Vue.createApp({
 		},
 		toggleShowBooks(){
 			this.showBooks = !this.showBooks;
+		},
+		handleEvent(e, data){
+			console.log(e, e.type);
+			if (data){
+				console.log(data);
+			}
+		},
+		handleMousemove(e){
+			this.x = e.offsetX;
+			this.y = e.offsetY;
+		},
+		toggleFav(book){
+			book.isFav = !book.isFav;
+		}
+	},
+
+	computed: {
+		filteredBooks(){
+			return this.books.filter((book) => book.isFav )
 		}
 	}
 
--- readme.md
+++ readme.md
@@ -5,3 +5,5 @@ Resources
 https://www.youtube.com/watch?v=F7PLPJqVotk
 
 
+https://www.youtube.com/watch?v=CYPZBK8zUik
+
